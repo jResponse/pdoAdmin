71aa0f18a8e6e5c90fc86ce6ad2c1e96d769831e
jResponse <atul@jreply.com>
2015-06-10 14:17:55 +0200

    Create msgs_en.js
    Dialog titles, alert and information messages in English.  Edit this file to create a UI in other languages.

---
+++ jsource/msgs_en.js
@@ -0,0 +1,114 @@
+var msgs_ = 
+{
+	badOffset:'Invalid table row offset',
+	badName:'The {nn} name may only contain [a-zA-Z0-9] and -',
+	badQueryURL:'Incomplete query URL',
+	binFormat:'Enter hexadecimal values for binary & varbinary types. The string length should be EVEN. ',
+	bitFormat:'Bitfield values should be a binary (0s & 1s) string. ',
+	blobMaxSize:'Blob data greater than 16Kb cannot be handled by the GUI',
+	
+	cannotEdit:'This table cannot be edited - it does not have a primary or unique key',
+	chooseFile:'Choose file',
+	clickToEdit:'Click entry to edit sets, enums,dates & datetimes ',
+	cloneMissing:'No clonable control found for field type {pyt}',
+	
+	dbsDown:'Could not access database server',
+	dbErr:'Database error {ansi}:{txt}',
+	dbExists:'A database bearing that name already exists',
+	dbFirst:'But first select a database!',
+	dbSelect:'Unable to select the database',
+	
+	
+	dbForUser:'Select a database for this user',
+	dbUnk:'Unknown database',
+	dbUnCreate:'Unable to create database',
+	
+	editField:'Edit Field Details',
+	editSQL:'Edit SQL',
+	emptyTable:'The table is empty',
+	
+	fieldNameInUse:'The name `{nf}` is being used by another field',
+	firstSelectDB:'First select a database!',
+
+	howToSelect:'To select a row without opening its editor hold the CTRL or SHIFT keys when you click',
+	
+	invUser:'Invalid user name',
+	invPostData:'Invalid POST data',
+	makeTable:'Make Table',
+	
+	nameTpl:'The {nf} name must be alphanumeric with {ln} or more characters. The 1st character must be alphabetic.',
+	needUniqueTableName:'That table name is already in use',
+	newDB:'New Database',
+	newUser:'New User',
+
+	noData:'No request data sent to server!',
+	noDropDB:'The database `{bd}` cannot be dropped',
+	noDropDBTable:'Tables from `{bd}` cannot be dropped',
+	noFields:'But first define some fields!',
+	noNewTablesHere:'You may not create new tables in `{bd}`',
+ noRowSelected:'But you have not selected anything!',
+	noSuchDB:'No such database on the server',
+	noSuchCharSet:'No such character set',
+	noSuchCollation:'No such collation',
+	noSuchPage:'Page number out of range',
+	noSuchTbl:'No such table in this database',
+	noSuchUser:'No such user',
+	noSQL:'But first enter some SQL to run!',
+ noTables:'No tables in this database!',
+	nothingHasChanged:'Nothing has changed!',
+	
+	opFailed:'Operation failed. Please try again.',
+	opSucceded:'Operation succeeded',
+	opRet:'Operation returned integer code {nn}',
+
+	pdoX:'PDO exception - {xx}',
+	pwdFmt:'The password must have 6 or more characters with at least one number, one lowercase letter & one uppercase letter',
+	results:'Result',
+	resultEmpty:'No result was returned',
+	rowEdit:'Table Row Editor',
+	
+	setEnumEmpty:'A valid member assignment is required',
+	sure:'Are you sure that you want to drop `{xx}`',
+	
+	tableData:'Table Contents',
+	tableFirst:'But first select a table!',
+	tdHint:'Click rows to edit. Hold the CTRL or SHIFT keys whilst clicking to select a row without opening the editor',
+
+	unDropDB:'Unable to drop `{bd}`',
+	unDropTable:'Unable to drop `{lbt}`',
+	unDropUser:'Unable to drop `{uu}`',
+	unMakeTable:'Unable to create the table',
+	unLists:'Unable to fetch DB & User Lists',
+	unRowEdit:'Unable to update row',
+	unTableData:'Unable to fetch table data',
+	unTableList:'Unable to fetch table list',
+	unTblStruc:'Unable to fetch table structure',
+	unUserData:'Unable to fetch user data',
+	
+	userAdded:'User added',
+	userFirst:'But first select a user!',
+	userNoDrop:'The user `{uu}` cannot be dropped',
+	
+	yearFormat:'Year values must be in the range [1901-2099]',
+	
+	itiny:'tinyint `{nf}` is invalid',
+	uitiny:'Unsigned tinyint `{nf}` is invalid',
+	ismall:'smallint `{nf}` is invalid',
+	uismall:'unsigned smallint `{nf}` is invalid',
+	imedium:'mediumint `{nf}` is invalid',
+	uimedium:'unsigned mediumint `{nf}` is invalid',
+	iint:'int `{nf}` is invalid',
+	uiint:'unsigned int `{nf}` is invalid',
+	ibig:'bigint `{nf}` is invalid',
+	uibig:'unsigned bigint `{nf}` is invalid',
+	
+	nfloat:'float `{nf}` is invalid',
+ nufloat:'unsigned float `{nf}` is invalid',
+ ndouble:'double `{nf}` is invalid',
+ nudouble:'unsigned double `{nf}` is invalid',
+ 
+ selAll:'Select all',
+ unSelAll:'Unseslect all',
+ setSelect:'Select Set Members',
+ enumSelect:'Select enumerated value', 
+}
