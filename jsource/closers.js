b4c6dbea4706536676fba0f7120e0dffe477bd66
jResponse <atul@jreply.com>
2015-06-10 14:11:18 +0200

    Create closers.js
    Relatively trivial functions used to close jQuery UI popup dialogs.  Placed in a separate file to stop them getting in the way.

---
+++ jsource/closers.js
@@ -0,0 +1,12 @@
+//---------------------------Dialog Closers & Before Closers--------------------------
+function closeMakeTableDialog(){$('#diaMakeTable').dialog('close');}
+
+function closeFieldEditor(){$('#diaFieldEdit').dialog('close');}
+
+function closeMakeUserDialog(){$('#diaMakeUser').dialog('close');}
+
+function closeMakeDBDialog(){$('#diaMakeDB').dialog('close');}
+
+function closeSQLEditor(){$('#diaSQL').dialog('close');}
+
+function closeRowEditor(){$('#diaRowEdit').dialog('close');}
